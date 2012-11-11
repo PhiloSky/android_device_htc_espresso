d23d87277cd47ff821bf0a7df7d8a97342562fb0
bluewater-cn <=>
2012-11-11 19:53:58 +0800

    change filemanager

--- espresso.mk
+++ espresso.mk
@@ -178,7 +178,7 @@ PRODUCT_COPY_FILES += \
 
 # File Manager
 PRODUCT_PACKAGES += \
-    FileManager
+    CMFileManager
 
 PRODUCT_COPY_FILES += \
     device/htc/espresso/vold.fstab:system/etc/vold.fstab \
